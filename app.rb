dd0366e224ce3382fc9e8cf53ea1de100908f494
Mori <keitamori@gmail.com>
2014-02-27 16:39:09 +0100

    Ajout de la racine ainsi que des controllers

--- app.rb
+++ app.rb
@@ -7,3 +7,30 @@ Braintree::Configuration.environment = :sandbox
 Braintree::Configuration.merchant_id = "use_your_merchant_id"
 Braintree::Configuration.public_key = "use_your_public_key"
 Braintree::Configuration.private_key = "use_your_private_key"
+
+
+get "/" do
+  erb :braintree
+end
+
+post "/create_transaction" do
+  result = Braintree::Transaction.sale(
+    :amount => "1000.00",
+    :credit_card => {
+      :number => params[:number],
+      :cvv => params[:cvv],
+      :expiration_month => params[:month],
+      :expiration_year => params[:year]
+    },
+    :options => {
+      :submit_for_settlement => true
+    }
+  )
+
+  if result.success?
+    "<h1>Success! Transaction ID: #{result.transaction.id}</h1>"
+  else
+    "<h1>Error: #{result.message}</h1>"
+  end
+end
+
