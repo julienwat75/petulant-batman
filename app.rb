829a2d894ac9da67713b2268bf341f7b59f30c4d
Mori <keitamori@gmail.com>
2014-02-27 16:34:35 +0100

    Ajout des fichier de configuration et des liens vers api Braintree

--- app.rb
+++ app.rb
@@ -1,3 +1,9 @@
 require 'sinatra' 
- require 'braintree' 
- require 'shotgun' 
+require 'braintree' 
+require 'shotgun' 
+
+
+Braintree::Configuration.environment = :sandbox
+Braintree::Configuration.merchant_id = "use_your_merchant_id"
+Braintree::Configuration.public_key = "use_your_public_key"
+Braintree::Configuration.private_key = "use_your_private_key"
